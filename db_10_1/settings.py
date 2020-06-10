76675320f8bf541f5fbee9e7ce6dba65882d66f6
SOFIAshyn <sophiya2petryshyn@gmail.com>
2020-06-10 17:18:50 +0300

    Compressed for WhiteNoice.

--- db_10_1/settings.py
+++ db_10_1/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'users.User'
